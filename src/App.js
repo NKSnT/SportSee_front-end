dfef9c7a390f2e62a8e61319cd5d45960504c4c1
NKSnT <bassinet.nahel@gmail.com>
2024-02-19 15:59:57 +0100

    usless part deleted

--- src/App.js
+++ src/App.js
@@ -2,32 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home/Home.jsx';
-//import User from './pages/User'; //asked for user data check from user/:id
-
-/*Asked :
-/user/:id/activity
-/user/:id/average-sessions
-/user/:id/performance
-*/
-
-//test with nested rout like so
-//import Activity from './pages/Activity';
-//import Performance from './pages/Performance';
-/* 
-<Route path="user/:id/" element={<User />}>
-      <Route path="activity" element={<Activity />} />
-      <Route path="performance" element={<Performance />} />
-</Route> 
-                    */
-
-/* not implemented
-//import About from './pages/Profil';
-//import Logement from './pages/settings';
-//import Logement from './pages/community'; 
-*/
-
-//maybe
-//import NoMatch from './pages/Error';
 
 import Header from './components/header/Header.jsx';
 
@@ -37,12 +11,7 @@ export default function App() {
             <Router>
                 <Header />
                 <Routes>
-                    <Route path="/user/:id" element={<Home />} />
-                    {/*  <Route path="user/:id/" element={<Home />}>
-                        <Route path="activity" element={<Activity />} />
-                        <Route path="average-sessions" element={<Average-sessions />} />
-                        <Route path="performance" element={<Performance />} />
-                    </Route> */}
+                    <Route path="/user/:id" element={<Home />} />                   
                 </Routes>
             </Router>
         </div>
